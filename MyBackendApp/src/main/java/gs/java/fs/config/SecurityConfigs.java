0f7b5b2259e3a1e4c249d8622cb0f77ca829c7f5
Cédric De Craim <cedricdecraim@msn.com>
2024-10-24 14:52:57 +0200

    suite et fin

--- MyBackendApp/src/main/java/gs/java/fs/config/SecurityConfigs.java
+++
@@ -1,7 +0,0 @@
-package gs.java.fs.config;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
-public interface SecurityConfigs {
-    void configure(AuthenticationManagerBuilder auth) throws Exception;
-}
--- MyBackendApp/src/main/java/gs/java/fs/exceptions/ClientException.java
+++
@@ -1,9 +0,0 @@
-package gs.java.fs.exceptions;
-
-    public class ClientException extends RuntimeException {
-
-        public ClientException(String message) {
-            super(message);
-        }
-    }
-
