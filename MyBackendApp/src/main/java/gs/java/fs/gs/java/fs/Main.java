c90b632b85ababe4c0646eba2e24e0238b344f6c
Cédric De Craim <cedricdecraim@msn.com>
2024-10-24 10:59:38 +0200

    spring dependency

--- MyBackendApp/src/main/java/gs/java/fs/gs/java/fs/Main.java
+++ MyBackendApp/src/main/java/gs/java/fs/gs/java/fs/Main.java
@@ -1,5 +1,4 @@
 package gs.java.fs;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
